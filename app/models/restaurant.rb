db93b3129e38b4adf012acf0fe0481370d59ba6f
Antoine Coulaud <antoinecoulaud@outlook.com>
2017-02-15 12:47:19 +0100

    create model review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :category, inclusion: { in: "chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   has_many  :reviews, dependent: :destroy
